2f3891cb11b07da6e0aab9ce0e88a1ea394559ca
joannisbs <joannisbs@gmail.com>
2020-04-02 14:42:01 -0300

    ADD: geral - configurando typescript

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+// node-typescript/src/server.ts
+import express from "express";
+
+const server = express();
+
+server.get("/", (_, res) => {
+  res.send("Hello ts-node!");
+});
+
+export default server;
