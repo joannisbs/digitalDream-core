e24dcaa2e16bc6a95a80720703cb4f39e3cba901
joannisbs <joannisbs@gmail.com>
2020-04-02 16:49:56 -0300

    ADD: Geral - base parser

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,39 @@
 // node-typescript/src/server.ts
 
 import express from "express";
+import logger from 'morgan';
+import * as bodyParser from 'body-parser';
 
-const server = express();
 
-server.get("/", (_, res) => {
+// Criando as configurações para o ExpressJS
+
+class App {
+  public express: express.Application;
+  constructor() {
+  this.express = express();
+  this.middleware();
+  this.routes();
+}
+
+
+private middleware(): void {
+  this.express.use(logger('dev'));
+  this.express.use(bodyParser.json());
+  this.express.use(bodyParser.urlencoded({ extended: false }));
+ }
+
+
+private routes(): void { 
+  let router = express.Router();
+  
+
+  router.get("/", (_, res) => {
   res.send("Hello ts-node!");
 });
+this.express.use('/', router);
+}
 
-export default server;
+
+}
+
+export default new App().express;
